740b90692b3f42b414c38fc0a0a6ce66a429365b
JehhB <jwy.bayod@gmail.com>
2024-07-16 22:02:02 +0800

    Removed unused php files and change error php file

--- calculate-age.php
+++
@@ -1,7 +0,0 @@
-<?php
-function calculate_age($birthday) {
-    $birthDate = new DateTime($birthday);
-    $currentDate = new DateTime();
-    $age = $currentDate->diff($birthDate)->y;
-    return $age;
-}
--- error.php
+++ error.php
@@ -10,19 +10,47 @@ http_response_code($err_code);
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title>Oops!</title>
+  <?php include __DIR__ . '/src/views/assets.php' ?>
+  <style>
+    .error-template {
+      padding: 40px 15px;
+      text-align: center;
+    }
+
+    .error-actions {
+      margin-top: 15px;
+      margin-bottom: 15px;
+    }
+
+    .error-actions .btn {
+      margin-right: 10px;
+    }
+  </style>
 </head>
 
 <body>
-  <div>
-    <strong>
-      <?php
-      echo $_SESSION['fatal_error']['message'] ?? 'Error 500: Internal server error';
-      unset($_SESSION['fatal_error']);
-      ?>
-    </strong>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <div class="error-template">
+          <h1>
+            Oops!</h1>
+          <h2>Error <?= $err_code ?></h2>
+          <div class="error-details">
+            <?php
+            echo $_SESSION['fatal_error']['message'] ?? 'Error 500: Internal server error';
+            unset($_SESSION['fatal_error']);
+            ?>
+          </div>
+          <div class="error-actions">
+            <a href="./" class="btn btn-primary btn-lg"> Take Me Home </a>
+            <a href="https://www.facebook.com/DICTCagayanValley" class="btn btn-outline-primary btn-lg"> Contact Support </a>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
-  <a href="/">Return home</a>
 </body>
 
 </html>
