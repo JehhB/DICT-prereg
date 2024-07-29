b604751902f2aecf3542d4f71c3532a0b40bb4ef
JehhB <jwy.bayod@gmail.com>
2024-07-29 12:49:00 +0800

    Added title

--- src/views/summary_info.php
+++ src/views/summary_info.php
@@ -7,7 +7,7 @@ $reg = Registration::find($_SESSION['auth_summary']);
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title>Preregistration</title>
   <?php include __DIR__ . '/assets.php' ?>
 </head>
 
