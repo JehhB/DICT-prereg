611fe8330859913b15ffcca98041b0d7bef144b2
JehhB <jwy.bayod@gmail.com>
2024-08-07 10:55:21 +0800

    Downloaded cdn file locally

--- README.md
+++ README.md
@@ -4,26 +4,38 @@ Preregistration website
 
 ## How to run (Docker)
 
-1. Edit email credentials by copying `/src/.env.template` to `/src/.env` and modify relevant fields
+1. Download CDN files locally
+
+```sh
+wget -i cdn.txt -P /assets/cdn/
+```
+
+2. Edit email credentials by copying `/src/.env.template` to `/src/.env` and modify relevant fields
 
 ```ini
 email=your.email@address.com
 password=your.email.password.recomended.to.use.app.key
 ```
 
-2.
+3.
 ```sh
 docker compose up
 ```
 
 ## How to run (XAMPP)
 
-1. Edit email credentials by copying `/src/.env.template` to `/src/.env` and modify relevant fields
+1. Download CDN files locally
+
+```sh
+wget -i cdn.txt -P /assets/cdn/
+```
+
+2. Edit email credentials by copying `/src/.env.template` to `/src/.env` and modify relevant fields
 
 ```ini
 email=your.email@address.com
 password=your.email.password.recomended.to.use.app.key
 ```
-2. Copy whole folder into htdocs
-3. Create new mysql database name `prereg`
-4. Run sql files from `database` in ascending order
+3. Copy whole folder into htdocs
+4. Create new mysql database name `prereg`
+5. Run sql files from `database` in ascending order
--- src/views/assets.php
+++ src/views/assets.php
@@ -1,12 +1,10 @@
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
-  integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+<link href="./assets/cdn/bootstrap.min.css" rel="stylesheet">
+<script src="./assets/cdn/popper.min.js"
   integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
   crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js"
+<script src="./assets/cdn/bootstrap.min.js"
   integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy"
   crossorigin="anonymous"></script>
-<script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.14.1/dist/cdn.min.js"></script>
-<link href=" https://cdn.jsdelivr.net/npm/bootswatch@5.3.3/dist/pulse/bootstrap.min.css " rel="stylesheet">
+<script defer src="./assets/cdn/cdn.min.js"></script>
 <link rel="stylesheet" href="./assets/style.css">
 <script src="./assets/script-filter-edit.js"></script>
